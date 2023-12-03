c9e74eacef2f5f3f641a83ba9fc695480113c276
margaret.lin <margaret.lin@moia.io>
2023-12-03 23:51:01 +0100

    Add Day01 Part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,25 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    val day = Day01(readInput("day01"))
+    println(day.part1())
+}
+
+class Day01(private val input: List<String>) {
+    fun part1(): Int {
+        // Ideas
+        // First Part:
+        // determine if String is Int
+        // 2 loops
+        // loop 1: count if first Int String, add to the sub-result
+        // loop 2: a reverse loop to check the last Int String, add to the sub-result
+
+        // Second Part:
+        // Sum all sub results and return the result
+        return input.sumOf { calculateRowDigits(it) }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    private fun calculateRowDigits(row: String): Int {
+        val firstDigit = row.first { it.isDigit() }
+        val secondDigit = row.last { it.isDigit() }
+        return "$firstDigit$secondDigit".toInt()
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
 }
